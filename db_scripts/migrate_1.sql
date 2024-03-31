ee4aa33574d23fa49cc4c6bf636567ac6fffd44a
JuniorOPoulet <152641835+JuniorOPoulet@users.noreply.github.com>
2024-03-31 13:21:23 -0400

    Update migrate_1.sql
    Migration

---
+++ db_scripts/migrate_1.sql
@@ -0,0 +1,18 @@
+ALTER TABLE singer RENAME TO musician;
+ALTER TABLE musician CHANGE singerName musicianName varchar(50);
+ALTER TABLE musician ADD role varchar(50);
+ALTER TABLE musician ADD bandName varchar(50);
+
+CREATE TABLE band (
+    bandName varchar(50) PRIMARY KEY,
+    creation YEAR,
+    genre varchar(50)
+);
+INSERT INTO band (bandName, creation, genre) VALUES ('Crazy Duo', 2015, 'rock');
+INSERT INTO band (bandName, creation, genre) VALUES ('Luna', 2009, 'classical');
+INSERT INTO band (bandName, creation, genre) VALUES ('Mysterio', 2019, 'pop');
+
+UPDATE musician SET role = 'vocals', bandName = 'Crazy Duo' WHERE musicianName = 'Alina';
+UPDATE musician SET role = 'guitar', bandName = 'Mysterio' WHERE musicianName = 'Mysterio';
+UPDATE musician SET role = 'percussion', bandName = 'Crazy Duo' WHERE musicianName = 'Rainbow';
+UPDATE musician SET role = 'piano', bandName = 'Luna' WHERE musicianName = 'Luna';
